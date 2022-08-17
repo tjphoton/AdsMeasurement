8512e13c2419f37cfac7ecf5f0083f4f1151cc6f
Xinjie Qiu <qiuxinjie@gmail.com>
2022-08-16 21:18:29 -0400

    add a new "in flight signal" page

---
+++ pages/4_🚦_In_Flight_Signal.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+st.set_page_config(page_title="Signal", page_icon="🚦")
+st.sidebar.header("In Flight Signals")
+
+st.title("In Flight Signals")
+
+st.markdown("#### Attention Metric")
+
+st.markdown("#### ")
+
+st.markdown("#### ")
+
+st.markdown("#### ")
+
+st.write("---")
+
+st.markdown(""" ###### © 2022 Xinjie Qiu ℠""")
---
+++ pages/5_🛠_Insights_Tool_Kit.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+st.set_page_config(page_title="Insights", page_icon="🛠")
+st.sidebar.header("Insights Tool Kit")
+
+st.title("Insights Tool Kit")
+
+st.markdown("#### Effect (What happened?)")
+
+st.markdown("#### Causality (Why happened?)")
+
+st.markdown("#### Counterfactual (What if?)")
+
+st.markdown("#### Forecasting (What next?)")
+
+st.write("---")
+st.markdown(""" ###### © 2022 Xinjie Qiu ℠""")
